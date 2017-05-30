6ed30412d428a8fab96659d4ec7d4eef5b6aed97
Ciaran McKenna <ciarantsc@gmail.com>
2017-05-30 09:14:46 +0100

    homework

---
+++ library/spec/library_spec.rb
@@ -0,0 +1,27 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../library')
+
+class TestLibrary < Minitest::Test
+
+def setup 
+  @library = Library.new[
+    {title: "Lord of The Rings",
+     rental_details: {student_name: "Ciaran", date: "29/05/17"}
+    },
+    {title: "Harry Potter"},
+    rental_details: {student_name: "John", date: "30/05/17"}
+    ]
+end
+
+def test_list_all
+  assert_equals(@library.list_all, @library)
+end
+
+
+
+
+
+
+
+end
